31845ae833f08f45ddb70c81bb4d543b6488d20a
SusanY05 <107528799+SusanY05@users.noreply.github.com>
2023-03-27 17:12:08 +0000

    Mon, Mar 27, 2023, 5:12 PM +00:00

--- js/counter.js
+++ js/counter.js
@@ -4,14 +4,14 @@ counters.forEach(counter => {
     counter.innerText = '0'
 
     const updateCounter = () => {
-        const target = + counter.getAttribute('data-target')
-        const c = counter.innerText
+        const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
 
         const increment = target / 200
 
         if (c < target) {
-            counter. innerText = `${Math.ceil (c+increment)}`
-            
+            counter.innerText = `${Math.ceil(c+increment)}`
+            setTimeout(updateCounter, 1)
         }else{
             counter.innerText = target
         }
